37fb41ae25fd239d6215a8e90f1e9f4619abe875
Maxim Fisyuk <maxim.fisyuk@gmail.com>
2022-06-12 20:53:59 +0300

    set fullscreen, add colors

--- supershape/sketch.js
+++ supershape/sketch.js
@@ -1,21 +1,37 @@
-const resolution = 50
+const resolution = 20
+const colorResolution = 100
 const globe = new Array(resolution + 1)
-let cam
+let myCamera
 
-let n1 = .2
-let n2 = 1.7
-let n3 = 1.7
-let m = 7.
-let a = 1.
-let b = 1.
+let shape1Params = {
+    n1: .2,
+    n2: 1.7,
+    n3: 1.7,
+    a: 1.,
+    b: 1.,
+    m: 7.
+}
+
+let shape2Params = {
+    n1: .2,
+    n2: 1.7,
+    n3: 1.7,
+    a: 1.,
+    b: 1.,
+    m: 7.
+}
+
+
+let saturation = 50
+let brightness = 90
 
 function setup() {
-    createCanvas(600, 400, WEBGL)
-    colorMode(HSB)
+    createCanvas(windowWidth, windowHeight, WEBGL)
+    colorMode(HSB, colorResolution, 100, 100, 1)
     for (let i = 0; i < resolution + 1; i++) {
         globe[i] = new Array(resolution)
     }
-    cam = createCamera();
+    myCamera = createCamera(0, 0, 0);
 }
 
 function superShape(theta, n1, n2, n3, a, b, m)
@@ -34,36 +50,33 @@ function draw() {
 
     orbitControl()
 
-    const r = 200;
-    rotateX(sin(frameCount * .01))
-    rotateY(cos(frameCount * .01))
+    const radius = windowHeight / 2;
+    rotateX(sin(frameCount * .002))
+    rotateY(cos(frameCount * .003))
 
     for (let lon = 0; lon < resolution + 1; lon++) {
         let theta = map(lon, 0, resolution, -PI, PI)
         for (let lat = 0; lat < resolution + 1; lat++) {
             let fi = map(lat, 0, resolution, -HALF_PI, HALF_PI)
 
-            let r1 = superShape(theta, n1, n2, n3, a, b, m)
-            let r2 = superShape(fi, n1, n2, n3, a, b, m)
+            let r1 = superShape(theta, ...Object.values(shape1Params))
+            let r2 = superShape(fi,    ...Object.values(shape2Params))
 
-            let x = r * r1 * cos(theta) * r2 * cos(fi)
-            let y = r * r1 * sin(theta) * r2 * cos(fi)
-            let z = r * r2 * sin(fi)
+            let x = radius * r1 * cos(theta) * r2 * cos(fi)
+            let y = radius * r1 * sin(theta) * r2 * cos(fi)
+            let z = radius * r2 * sin(fi)
             globe[lon][lat] = new p5.Vector(x, y, z)
         }
     }
 
     for (let i = 0; i < resolution; i++) {
-        // let hu = map(i, 0, resolution, 0, 255)
-        // fill(hu % 255, 255, 255)
+        let hu = map(i, 0, resolution, 0, colorResolution)
         beginShape(TRIANGLE_STRIP)
         // beginShape(POINTS)
         for (let j = 0; j < resolution + 1; j++) {
             let v1 = globe[i][j]
             let v2 = globe[i+1][j]
-            stroke(255)
-            strokeWeight(.5)
-
+            stroke(hu, saturation, brightness)
             vertex(v1.x, v1.y, v1.z)
             vertex(v2.x, v2.y, v2.z)
         }
